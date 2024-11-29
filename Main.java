3cc33068d36bbd28c725d1c6a9445def18d2083e
Sukhveer Singh <44468231+veersingh9540@users.noreply.github.com>
2024-11-29 11:19:15 -0500

    Data Types added

---
+++ Main.java
@@ -0,0 +1,19 @@
+class dataTypes
+{
+    public static void main(String a[]){
+        float marks = 6.5f;
+        double marks2 = 100.33;
+        char c = 'k';
+        String Name= "Sukhveer Singh";
+        Boolean IsJavaGood = true;
+        Boolean IsJavaNotGood = false;
+
+        System.out.println("Hello Below are some data types of Java");
+        System.out.println("This is float value:  " + marks);
+        System.out.println("Thsi is double value: " + marks2);
+        System.out.println("This is String: " + Name);
+        System .out.println("This is char: "+ c);
+        System.out.println("This is boolean (is java not good?) " + IsJavaNotGood);
+        System.out.println("This is boolean (is java good?) " + IsJavaGood);
+    }
+}
