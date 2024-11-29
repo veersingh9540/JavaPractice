6fbb81510073d8a44be4910599f01b1feb47889b
Sukhveer Singh <44468231+veersingh9540@users.noreply.github.com>
2024-11-29 11:28:03 -0500

    Added more data types

--- Main.java
+++ Main.java
@@ -1,16 +1,28 @@
 class dataTypes
 {
     public static void main(String a[]){
+        /* This is Integer Data Types */
+        int Marks = 100;
+        byte Bytes = 125;
+        long l = 54324l;
+        short s = 1234;
         float marks = 6.5f;
         double marks2 = 100.33;
+        /* This is Words and String Data Types */
         char c = 'k';
         String Name= "Sukhveer Singh";
-        Boolean IsJavaGood = true;
-        Boolean IsJavaNotGood = false;
+        boolean IsJavaGood = true;
+        boolean IsJavaNotGood = false;
 
+        /*Printing to Console all the Data types */
         System.out.println("Hello Below are some data types of Java");
+        System.out.println("This is Integer value: "+ Marks);
+        System.out.println("This is Byte Value: "+ Bytes);
+        System.out.println("This is Long Value: "+ l);
+        System.out.println("This is Short value: " + s);
         System.out.println("This is float value:  " + marks);
         System.out.println("Thsi is double value: " + marks2);
+
         System.out.println("This is String: " + Name);
         System .out.println("This is char: "+ c);
         System.out.println("This is boolean (is java not good?) " + IsJavaNotGood);
